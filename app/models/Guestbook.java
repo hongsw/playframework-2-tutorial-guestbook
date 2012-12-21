24308351ba2e772169d62c9af5695ff413eeba29
hongsw <ajinsys@ajinsys.com>
2012-12-21 17:49:49 +0900

    소스수정

--- app/models/Guestbook.java
+++ app/models/Guestbook.java
@@ -6,50 +6,33 @@ import javax.persistence.*;
 import org.apache.commons.codec.digest.DigestUtils;
 
 import play.db.ebean.*;
-import play.data.format.Formats.*;
-import play.data.validation.Constraints.*;
+import play.data.validation.*;
 
 @Entity 
 @Table(name="guestbook")
 public class Guestbook extends Model {
     
     @Id
-	@GeneratedValue(strategy = GenerationType.AUTO) 
     public Long id;
 
-    @Required
-    @MinLength(4)
+    @Constraints.MinLength(4)
     public String name;
     
-    @Required
-    @Email
+    @Constraints.Email
     public String email;
     
-    @Required
-    @MinLength(6)
+    @Constraints.Required
     public String password;
 
-    @NonEmpty
     public String content;
-    
-    @DateTime(pattern="MM/dd/yy")
     public Date postAt;
-    
-    // -- Querie  //
     public static Model.Finder<Long,Guestbook> find = new Model.Finder<Long, Guestbook>(Long.class, Guestbook.class);
-
-	public Guestbook(String name, String email, String password, String content) {
-		this.name = name;
-		this.email = email;
-		this.password = password;
-		this.content = content;
-	}
-
-	public static Guestbook findbyIDPW(Long id2, String password2) {
+    public static Guestbook findbyIDPW(Long id2, String password2) {
 		return find.where()
 	            .eq("email", id2)
 	            .eq("password", DigestUtils.md5Hex( password2 ))
 	            .findUnique();
 	}
+    
     
 }
